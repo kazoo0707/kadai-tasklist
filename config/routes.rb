24fc3a6acf5ae51e329cdc58d16665b7173fc968
kazoo.teru <kkt0192837465@gmail.com>
2018-10-27 09:50:01 +0000

    fixed kadai-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
