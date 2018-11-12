b0f4a349331346c60632fe186d7e246fd933be11
kazoo.teru <kkt0192837465@gmail.com>
2018-11-12 05:43:12 +0000

    fixed kadai12

--- db/migrate/20181106112354_add_user_to_tasks.rb
+++ db/migrate/20181106112354_add_user_to_tasks.rb
@@ -1,13 +1,5 @@
 class AddUserToTasks < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-
-      t.timestamps
-      
     add_reference :tasks, :user, foreign_key: true
-    end
   end
 end
